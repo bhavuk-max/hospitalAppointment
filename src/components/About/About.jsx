13133a8aadb3ab5b8559be9583ad8908dc532aff
keshav sehgal <keshavsehgal27@gmail.com>
2021-10-23 13:51:01 +0530

    about us

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,24 @@
 import React, { Component } from "react";
+import "./About.css";
+import doc from "./doc.jpg";
 class About extends React.Component {
   render() {
     return (
       <div>
-        <h1>About-Us</h1>
+        <h1>About Max Care</h1>
+        {/* <div className="quote">
+          <img src={quote} height="" />
+        </div> */}
+        <h1 className="quote">Enhancing life Excelling in care</h1>
+        <h2>Our Aim</h2>
+        <span className="aim">
+          Max Care is on a mission to make quality healthcare affordable and
+          accessible virtually. We believe in empowering our users with the most
+          accurate, comprehensive, and curated information and care, enabling
+          them to make better healthcare decisions. Our users safety is our top
+          priority
+        </span>
+        <img src={doc} width="40%" />
       </div>
     );
   }
--- src/components/Articles/Article.jsx
+++ src/components/Articles/Article.jsx
@@ -1,9 +1,71 @@
 import React, { Component } from "react";
+import health from "./health.jpg";
+import Card from "react-bootstrap/Card";
+import Carousel from "react-bootstrap/Carousel";
+import "./Article.css";
 class Article extends React.Component {
   render() {
     return (
       <div>
         <h1>Articles</h1>
+
+        <Carousel>
+          <Carousel.Item className="carousel-item header">
+            {/* <img className="d-block w-100" src={first} alt="First slide" /> */}
+            <div className="inner-card">
+              <Card style={{ width: "18rem", margin: "10px" }}>
+                <Card.Body>
+                  <Card.Title>Cardiac</Card.Title>
+                  <Card.Img variant="top" src={health} className="image" />
+                  <Card.Text>
+                    "Healthy living" to most people means both physical and
+                    mental health are in balance or functioning well together in
+                    a person. In many instances, physical and mental health are
+                    closely linked, so that a change (good or bad) in one
+                    directly affects the other. Consequently, some of the tips
+                    will include suggestions for emotional and mental "healthy
+                    living."
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+              <Card style={{ width: "18rem", margin: "10px" }}>
+                <Card.Body>
+                  <Card.Title>Dental</Card.Title>
+                  <Card.Img variant="top" src={health} className="image" />
+                </Card.Body>
+              </Card>
+              <Card style={{ width: "18rem", margin: "10px" }}>
+                <Card.Body>
+                  <Card.Title>ENT</Card.Title>
+                  <Card.Img variant="top" src={health} className="image" />
+                </Card.Body>
+              </Card>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item className="carousel-item header">
+            {/* <img className="d-block w-100" src={first} alt="First slide" /> */}
+            <div className="inner-card">
+              <Card style={{ width: "18rem", margin: "10px" }}>
+                <Card.Body>
+                  <Card.Title>Dermatology</Card.Title>
+                  <Card.Img variant="top" src={health} className="image" />
+                </Card.Body>
+              </Card>
+              <Card style={{ width: "18rem", margin: "10px" }}>
+                <Card.Body>
+                  <Card.Title>Orthopedic</Card.Title>
+                  <Card.Img variant="top" src={health} className="image" />
+                </Card.Body>
+              </Card>
+              <Card style={{ width: "18rem", margin: "10px" }}>
+                <Card.Body>
+                  <Card.Title>Gastroenterology</Card.Title>
+                  <Card.Img variant="top" src={health} className="image" />
+                </Card.Body>
+              </Card>
+            </div>
+          </Carousel.Item>
+        </Carousel>
       </div>
     );
   }
