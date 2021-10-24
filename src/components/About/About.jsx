d208fac2da5a3c707d222f496cfed2e492f027de
Pranav Sehgal <pranavsehgal12@gmail.com>
2021-10-24 19:37:32 +0530

    Article and about us

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,15 +10,49 @@ class About extends React.Component {
           <img src={quote} height="" />
         </div> */}
         <h1 className="quote">Enhancing life Excelling in care</h1>
-        <h2>Our Aim</h2>
-        <span className="aim">
-          Max Care is on a mission to make quality healthcare affordable and
-          accessible virtually. We believe in empowering our users with the most
-          accurate, comprehensive, and curated information and care, enabling
-          them to make better healthcare decisions. Our users safety is our top
-          priority
-        </span>
-        <img src={doc} width="40%" />
+
+        <section className="aimSection">
+          <h2>Our Aim</h2>
+          <div className="aim">
+            Max Care is on a mission to make quality healthcare affordable and
+            accessible virtually. We believe in empowering our users with the
+            most accurate, comprehensive, and curated information and care,
+            enabling them to make better healthcare decisions. Our users safety
+            is our top priority
+          </div>
+        </section>
+        <section className="docImage">
+          <h1>Content</h1>
+          {/* <img src={doc} alt="doc" width="40%" /> */}
+        </section>
+        <section className="offering">
+          <h2>What we offer</h2>
+          <div className="aboutUs">
+            <div>
+              <li className="about">
+                Maintain a safe distance from others, even if they don’t appear
+                to be sick.
+              </li>
+              <li className="about">
+                Wear a mask in public, especially indoors or when physical
+                distancing is not possible.
+              </li>
+            </div>
+            <div>
+              <li className="about">
+                Choose open, well-ventilated spaces over closed ones. Open a
+                window if indoors.
+              </li>
+              <li className="about">
+                Clean your hands often. Use soap and water, or an alcohol-based
+                hand rub.
+              </li>
+            </div>
+          </div>
+        </section>
+        <footer className="footer">
+          <h5>Copyright © 2021, Max Care All rights reserved.</h5>
+        </footer>
       </div>
     );
   }
---
+++ src/components/Articles/ArticlesData.js
@@ -0,0 +1,38 @@
+import health from "./health.jpg";
+const carusel1 = [
+  {
+    title: "Cardiac",
+    body: " Healthy living to most people means both physical and ental health are in balance or functioning well together in a person.",
+    image: health,
+  },
+  {
+    title: "Dental",
+    body: "",
+    image: health,
+  },
+  {
+    title: "ENT",
+    body: "",
+    image: health,
+  },
+];
+
+const carusel2 = [
+  {
+    title: "Dermatology",
+    body: "",
+    image: health,
+  },
+  {
+    title: "Orthopedic",
+    body: "",
+    image: health,
+  },
+  {
+    title: "Gastroenterology",
+    body: "",
+    image: health,
+  },
+];
+
+export const articles = [carusel1, carusel2];
