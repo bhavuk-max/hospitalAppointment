c8ce23df94e36da1ab69913aed9fef3c44c7ca5b
unknown <bgupta1521.ca19@chitkara.edu.in>
2021-11-06 15:12:31 +0530

    push1

---
+++ src/components/mycomponent/Pharmacy/index2.js
@@ -0,0 +1,8 @@
+import prom from "./promethazine.jpg";
+export const companyData = [
+  {
+    id: 1,
+    company: "Photosnap",
+    logo: prom,
+  },
+];
--- src/components/mycomponent/Pharmacy/photos.js
+++ src/components/mycomponent/Pharmacy/photos.js
@@ -1,19 +1,4 @@
-import React, { Component } from 'react';
-import crocin from "../Pharmacy/crocin.png";
-import photo2 from "./photo2.jfif";
-// export const photos=[{
-// Name:"crocin",
-// source:"../Pharmacy/crocin.png",
-// width: "12rem",
-//  height: "20rem",
-//  alt: "crocin tablets" 
-// },
-// {
-//     Name:"Lisinopril",
-//     source:"./photo2.jfif",
-//     width: "12rem",
-//      height: "20rem",
-//      alt: "Lisinopril tablets" 
-//     }
-// ]
-//export default photos;
+// const IMAGES={
+//     image1: require('/crocin.png').default,
+//     image2: require('/photo2.jfif').default,
+// }
